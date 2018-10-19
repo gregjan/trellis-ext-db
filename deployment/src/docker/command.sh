96370e6f9572d73afdd4db35f594011bd1a57929
Gregory Jansen <gregory.jansen@gmail.com>
2018-10-19 10:41:46 -0400

    moved logging to the docker console for docker build

--- deployment/src/docker/command.sh
+++ deployment/src/docker/command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 10
-/opt/trellis/bin/trellis-db db migrate /opt/trellis/etc/config.yml
-/opt/trellis/bin/trellis-db server /opt/trellis/etc/config.yml
+/opt/trellis/bin/trellis-db db migrate /config.yml
+/opt/trellis/bin/trellis-db server /config.yml
